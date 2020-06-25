d687d5e6020184669a82c92569075bad49b5931a
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2020-06-25 21:57:50 +0300

    create all required activities

---
+++ app/src/main/java/com/s/emergency_app/Passenger.java
@@ -0,0 +1,13 @@
+package com.s.emergency_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Passenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger);
+    }
+}
