018153b65bc7f51f5c0ed9286b8ca3cccd3d98c2
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2020-06-26 12:00:10 +0300

    add layout design for passenger,client and technician login activities

--- app/src/main/java/com/s/emergency_app/LoginActivity.java
+++ app/src/main/java/com/s/emergency_app/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.s.emergency_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+    }
+
+    public void startTechnicianActivity(View v)
+    {
+        Intent intent = new Intent(v.getContext(), TechnicianActivity.class);
+        startActivity(intent);
     }
 }
